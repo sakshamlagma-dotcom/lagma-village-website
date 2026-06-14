9cad56eb07f61abfaa898008f735fbc7ad005905
sakshamlagma-dotcom <sakshamlagma-dotcom@users.noreply.github.com>
2026-06-14 20:23:35 +0530

    Configure Firebase notes project

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 window.LAGMA_FIREBASE_CONFIG = {
-  apiKey: "",
-  authDomain: "",
-  projectId: "",
-  storageBucket: "",
-  messagingSenderId: "",
-  appId: ""
+  apiKey: "AIzaSyAUkYG6pWWUMLYPKwRq62hjsogHN-1JmqA",
+  authDomain: "lagma-village-notes.firebaseapp.com",
+  projectId: "lagma-village-notes",
+  storageBucket: "lagma-village-notes.firebasestorage.app",
+  messagingSenderId: "7909835731",
+  appId: "1:7909835731:web:8fb01cd3611c4f70f5e526"
 };
 
 window.LAGMA_FIREBASE_OPTIONS = {
