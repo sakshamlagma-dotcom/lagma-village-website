94c357623f3d6f324e562f8458053950a661bad5
sakshamlagma-dotcom <sakshamlagma-dotcom@users.noreply.github.com>
2026-07-06 15:46:57 +0530

    add local service icons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lagma-village-v36";
+const CACHE = "lagma-village-v37";
 const ASSETS = [
   "./",
   "./index.html",
@@ -23,6 +23,8 @@ const ASSETS = [
   "./upload.js?v=1",
   "./firebase-config.js?v=8",
   "./analytics.js?v=3",
+  "./assets/lucide.min.js?v=1",
+  "./lagma-icons.js?v=1",
   "./manifest.webmanifest?v=6",
   "./sj-logo.webp",
   "./a.webp",
